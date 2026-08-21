d3527be5af9c7c015c4677ff6722045c64edcfe3
wng777-cmd <wng777@naver.com>
2026-08-21 10:12:21 +0900

    Remove unused audit bootstrap helper

--- scripts/site-audit-bootstrap-20260821.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  if(!document.querySelector('link[data-site-audit-css]')){
-    var l=document.createElement('link');
-    l.rel='stylesheet';
-    l.href='scripts/site-audit-fixes-20260821.css?v=20260821audit1';
-    l.setAttribute('data-site-audit-css','1');
-    document.head.appendChild(l);
-  }
-})();
